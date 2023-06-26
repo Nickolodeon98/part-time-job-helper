60a9689d93332229b5deaed051356f268cfda282
Nickolodeon98 <sjeon0730@gmail.com>
2023-06-26 21:53:45 +0900

    feat: add a new project

---
+++ src/main/java/com/example/parttimejobhelper/PartTimeJobHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.parttimejobhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartTimeJobHelperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PartTimeJobHelperApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/parttimejobhelper/PartTimeJobHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parttimejobhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartTimeJobHelperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
